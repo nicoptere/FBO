63b101538720be0cfd533af5f1d988bcd1fd0e34
nicolas barradeau <barradeau@gmail.com>
2016-01-11 17:06:43 +0100

    +image

---
+++ glsl/image/simulation_vs.glsl
@@ -0,0 +1,5 @@
+varying vec2 vUv;
+void main() {
+    vUv = vec2(uv.x, uv.y);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
--- glsl/morph/render_fs.glsl
+++ glsl/morph/render_fs.glsl
@@ -1,4 +1,4 @@
 void main()
 {
-    gl_FragColor = vec4( 1. );
+    gl_FragColor = vec4( vec3( 1. ), .25 );
 }
